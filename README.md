7228c4149eb86e1ce000986b3656d3ef008cddc8
Sanjida Islam Shohely <66716089+shohely1710@users.noreply.github.com>
2023-04-04 16:37:43 +0600

    slick slider right to left

---
+++ README.md
@@ -0,0 +1,1 @@
+# practice-slick-slider-rtl
---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+ 
+  
+  $('.carousel-rtl').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrow: true,
+    dots: true,
+    rtl: true,
+  });
+});
